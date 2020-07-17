a5f5f7d5dc51e31994556f73b2ec8ae829e58c2c
Bogdan Gradinariu <bogdan.gradinariu@gmail.com>
2020-07-17 15:06:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import Sticky from './sticky';
 
-const install = function(Vue) {
-  Vue.directive('Sticky', Sticky);
-};
-
-if (window.Vue) {
-  Vue.use(install);
-}
-
-Sticky.install = install;
-
 export default Sticky;
